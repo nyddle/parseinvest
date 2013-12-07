3324d3f5b3f6b3dc07420aeee09aa7eeace75c20
Albert Besuhoff <serg@pereborstudio.com>
2013-12-08 00:31:21 +0200

    - Moved REST API to external pereborstudio.com domain

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-rest_api = "http://redmine.pereborstudio.com/investeka.php"
+rest_api = "http://pereborstudio.com/investeka.php"
 orgs = [
     { "name": "Альфа капитал", "link": "http://www.alfacapital.ru/rss/news/", "feedtype": "rss" },
     { "name": "ItInvest", "link": "http://www.itinvest.ru/rss/news/", "feedtype": "rss" },
